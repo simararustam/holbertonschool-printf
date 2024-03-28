0550af37334bfbca8bb65f7ae9bb7eb5617bdb9d
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-28 16:05:45 +0400

    Create main.h

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/*Function prortypes writed here*/
+int _itoa(int value);
+int _printf(const char *format, ...);
+int _putchar(char c);
+int _puts(char *str);
+int _putsnumber(char *str);
+int print_number(unsigned int r, int s);
+#endif
